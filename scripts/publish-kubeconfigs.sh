715b95ec9f9f717158dcfd620900e54bfe559a67
Stephane REYTAN <srnfr@users.noreply.github.com>
2023-08-21 17:23:41 +0200

    Suffix kubeconfig with .yaml

--- scripts/publish-kubeconfigs.sh
+++ scripts/publish-kubeconfigs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Cleanup
+##Cleanup wole bucket
 for f in $(s3cmd ls s3://kconfig/ | awk '{print $4}'); do
   s3cmd rm $f
 done
@@ -9,15 +9,15 @@ done
 for c in $(doctl kubernetes clusters list --format Name | grep -v Name ); do
     if [[ "$c" != "Name" ]]; then
         echo "$c"
-        doctl kubernetes cluster kubeconfig show $c > $c.kubeconfig
+        doctl kubernetes cluster kubeconfig show $c > $c.kubeconfig.yaml
     fi
 done;
 
 ##Upload
-s3cmd put *.kubeconfig s3://kconfig/
+s3cmd put *.kubeconfig.yaml s3://kconfig/
 
 ## Clean local files
-rm -f *.kubeconfig
+rm -f *.kubeconfig.yaml
 
 ##
 echo "NE PAS OUBLIER DE RENDRE PUBLIC LES KUBECONFIG"
