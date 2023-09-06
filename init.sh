a05ec95a99c3bbd49d699fc2319131949151c66d
Stephane REYTAN <srnfr@users.noreply.github.com>
2023-09-06 19:54:35 +0000

    2nd fix for CodeSpaces

--- init.sh
+++ init.sh
@@ -19,8 +19,10 @@ ENTROPY=$2
 URL="https://kconfig.fra1.digitaloceanspaces.com/k8-do-grp${GRP}-${ENTROPY}.kubeconfig.yaml"
 echo "Downloading $URL ..."
 
-[ ! -d "~/.kube" ] && mkdir ~/.kube
+[ ! -d ~/.kube ] && mkdir ~/.kube
 wget -nv $URL -O ~/.kube/config
+
+echo "Securing access to kubeconfig"
 chmod o-r ~/.kube/config
 chmod g-r ~/.kube/config
 
@@ -34,6 +36,7 @@ fi
 ## Install Cilium et autres
 if [ -f /usr/local/bin/cilium ]; then
   echo "Cilium deja installé"
+
 else
   export CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
   export GOOS=linux
@@ -42,20 +45,39 @@ else
   sha256sum --check cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum
   sudo tar -C /usr/local/bin -xzvf cilium-${GOOS}-${GOARCH}.tar.gz
   rm -f cilium-*.tar.gz{,.sha256sum}
-
-  kubectl krew install cilium
-  kubectl krew index add kvaps https://github.com/kvaps/krew-index
-  kubectl krew install kvaps/node-shell
-
-  for i in $(kubectl get node -o json | jq -r '.items[].metadata.name'); do
-	  kubectl cilium exec $i cilium version
-	  kubectl cilium exec $i cilium status
-  done
-  kubectl krew install stern
 fi
 
 cilium version
 cilium status
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+## Install Krew
+if ! kubectl krew version ; then
+  (
+  echo "Installing krew.." &&
+  cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+  )
+
+  kubectl krew install cilium
+  kubectl krew index add kvaps https://github.com/kvaps/krew-index
+  kubectl krew install kvaps/node-shell
+  kubectl krew install stern
+
+else
+  echo "krew et ses modules deja installés"
+fi
+
+for i in $(kubectl get node -o json | jq -r '.items[].metadata.name'); do
+	  kubectl cilium exec $i cilium version
+	  kubectl cilium exec $i cilium status
+done
 
 echo "#-----------------"
 echo "Votre groupe : ${GRP}"
