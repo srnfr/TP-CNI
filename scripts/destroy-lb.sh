a27968fddc514680ba1ba12474deac1c08bf8cbc
srnfr <stephane@reytan.fr>
2023-09-08 16:32:13 +0200

    Add lb-groupe support

--- scripts/destroy-lb.sh
+++ scripts/destroy-lb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for d in $(doctl compute load-balancer list --format 'Name,ID' --no-header -o json | jq -r '.[] | "\(.name),\(.id)"' | egrep '^a[0-9a-z]+,' ); do
+for d in $(doctl compute load-balancer list --format 'Name,ID' --no-header -o json | jq -r '.[] | "\(.name),\(.id)"' | egrep '^(a[0-9a-z]+|lb-groupe[0-9]+),' ); do
 	echo "LB= $d"
 	id=$(echo $d | cut -f2 -d',')
 	echo "ID= $id"
