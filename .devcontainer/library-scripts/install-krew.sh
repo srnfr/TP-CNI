7524a44491f6e7c16f9a94f25ae8f6b6bbed3729
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-03-03 16:52:17 +0000

    Modifiy vscontainer for initial install of krew

---
+++ .devcontainer/library-scripts/install-krew.sh
@@ -0,0 +1,8 @@
+RUN set -x; cd "$(mktemp -d)" && \
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" && \
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" && \
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" && \
+    tar zxvf krew.tar.gz && \
+    KREW=./krew-"${OS}_${ARCH}" && \
+    "$KREW" install krew && \
+    echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> /home/vscode/.bashrc
