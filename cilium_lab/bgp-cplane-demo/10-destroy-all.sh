dac7138a390dd05a0d850f9be2994b7ba1613478
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-03-04 08:20:16 +0100

    Update 10-destroy-all.sh

--- cilium_lab/bgp-cplane-demo/10-destroy-all.sh
+++ cilium_lab/bgp-cplane-demo/10-destroy-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-containerlab destroy clusters -t /mnt/cilium_lab/bgp-cplane-demo/topo.yaml 
+containerlab destroy clusters -t topo.yaml 
 for c in $(kind get clusters); do
 	kind delete cluster --name $c
 done
