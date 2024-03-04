b153a35fd3ce35184f357ee444d57a100989ede3
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-03-04 08:19:45 +0100

    bgp : fix cilium version

--- cilium_lab/bgp-cplane-demo/03-install-cilium.sh
+++ cilium_lab/bgp-cplane-demo/03-install-cilium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm upgrade --install --namespace kube-system --repo https://helm.cilium.io cilium cilium --values values.yaml
+helm upgrade --install --namespace kube-system --repo https://helm.cilium.io cilium cilium --values values.yaml  --version 1.12.19
 
 #kubectl rollout restart daemonset cilium -n kube-system
 
