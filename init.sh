95684fdfccf63af507cf67eec3f743de8fb3e1bd
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-03-05 17:34:19 +0000

    Hubble cli install

--- init.sh
+++ init.sh
@@ -57,10 +57,10 @@ else
   HUBBLE_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/hubble/master/stable.txt)
   HUBBLE_ARCH=amd64
   if [ "$(uname -m)" = "aarch64" ]; then HUBBLE_ARCH=arm64; fi
-  curl -L --fail --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
+  curl -s -L --fail --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
   sha256sum --check hubble-linux-${HUBBLE_ARCH}.tar.gz.sha256sum
   sudo tar xzvfC hubble-linux-${HUBBLE_ARCH}.tar.gz /usr/local/bin
-  hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
+  rm -f hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
 fi
 
 
