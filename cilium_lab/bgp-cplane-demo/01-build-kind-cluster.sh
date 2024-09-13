6614156172dbbe5618082f77e8354d33fe663310
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-09-13 13:59:03 +0200

    k8s v1.30.4

--- cilium_lab/bgp-cplane-demo/01-build-kind-cluster.sh
+++ cilium_lab/bgp-cplane-demo/01-build-kind-cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+VERSION_K8S="1.30.4"
 echo "---"
-kind create cluster -v=2 --config=./kind-config.yaml --image kindest/node:v1.24.17
+kind create cluster -v=2 --config=./kind-config.yaml --image kindest/node:v${VERSION_K8S}
 echo -n "Kind running clusters :"
 kind get clusters
 
