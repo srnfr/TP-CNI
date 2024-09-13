a84245fe54598bedb2815503d565348ea47fd356
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-09-13 17:13:03 +0200

    Update destroy-droplet.sh

--- scripts/destroy-droplet.sh
+++ scripts/destroy-droplet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for d in $(doctl compute droplet list --format 'Name' | grep ciliumlab-); do
+for d in $(doctl compute droplet list --format 'Name' | grep "ciliumlab-"); do
 	echo $d
 	doctl compute droplet delete -f $d
 done
