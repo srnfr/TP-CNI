3740f839de744fc7d6bf937fc359a8b818fe8cc6
Stephane REYTAN <srnfr@users.noreply.github.com>
2024-12-15 15:03:49 +0100

    better ingress

--- scripts/install-nginx-ingress.sh
+++ scripts/install-nginx-ingress.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+source ./.env
+
+if ([ -z "${CTX_K8S}" ]); then
+	echo "CTX_K8S  not set, using default"
+	CTX_K8S="default"
+fi
 
 echo "Install de nginx ingress de la marketplace DO sur :"
 
-for c in $(doctl kubernetes clusters list --format ID | grep -v ID ); do
+for c in $(doctl kubernetes clusters list --format ID --context $CTX_K8S | grep -v ID ); do
     if [[ "$c" != "Name" ]]; then
         echo "- $c"
-	doctl kubernetes 1-click install $c --1-clicks nginx-ingress
+	doctl kubernetes 1-click install $c --1-clicks nginx-ingress --context $CTX_K8S
     fi
 done;
